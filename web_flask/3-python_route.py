d7ce1b7fc61b6e2d03b18f3d6021f348b1c14b89
JoseMoreno00 <accgitjosem@gmail.com>
2023-12-11 09:28:04 -0800

    Adding route that display “Python ” and following a text | Task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -25,6 +25,7 @@ def c_text(text):
 
 
 @app.route('/python/<text>', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 def python_text(text="is cool"):
     text = text.replace("_", " ")
     return f"Python {text}"
